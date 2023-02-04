1e068f858f2c7c747be913a4f0d069c4659454a2
YauheniKazlou <zenka.2012@mail.ru>
2023-02-04 15:01:35 +0300

    Числа

--- New file.py
+++ New file.py
@@ -1,3 +1,17 @@
-str = '123'
-str1 = int(str)
-print(type(str1))
+import math
+print(1**100)
+print(2**16)
+print(2-100)
+print((2+2)*2)
+print(round(60/3))
+print(round(math.sqrt(144)))
+x=2+5*14
+print(x)
+x1=66-11
+print(x1)
+x2=math.sqrt(15-8)
+print(round(x2, 3))
+
+
+
+
