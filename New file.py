eaa3e65f8400a184a5898ce6d3bb7d48b6f15f7c
YauheniKazlou <zenka.2012@mail.ru>
2023-02-07 18:13:43 +0300

    Условные операторы - ДР про восраст ( - года, лет).

--- New file.py
+++ New file.py
@@ -1,21 +1,11 @@
 n = input("Введите свой возраст:")
 
-
 if int(n) < 1:
     print("Возраст введен неверно")
 elif int(n) > 99:
     print("Возраст введен неверно")
-elif int(n) == 1:
-    print("Мне", n, "год")
-elif int(n) == 2:
-    print("Мне", n, "года")
-elif int(n) == 3:
-    print("Мне", n, "года")
-elif int(n) == 4:
-    print("Мне", n, "года")
 elif int(n) == 11:
     print("Мне", n, "лет")
-
 elif int(n[-1]) == 1:
     print("Мне", n, "год")
 elif int(n[-1]) == 2:
