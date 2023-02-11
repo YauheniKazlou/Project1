cd6bb9913f7fdafe1cda7717debed6e705c738cf
YauheniKazlou <zenka.2012@mail.ru>
2023-02-11 21:54:04 +0300

    ДР - Обработка исключений

--- New file.py
+++ New file.py
@@ -1,10 +1,12 @@
-
-
 try:
     chislo1 = input("Введите сегодняшний день недели: ")
     chislo2 = input("Введите сегодняшнее число: ")
-    print("Сегодня: " + chislo1 + " " + int(chislo2) + "-го")
-except TypeError:
-    print("Используются разные типы данных в функции!")
+    print("Сегодня:", str.capitalize(chislo1), chislo2 + "-го")
 except ValueError:
     print("Введена строка вместо цифр!")
+try:
+    chislo1 = input("Введите первое число: ")
+    chislo2 = input("Введите второе число: ")
+    print("Результат:" + int(chislo1) + int(chislo2))
+except TypeError:
+    print("Используются разные типы данных в функции!")
