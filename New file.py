110625e4c63a78c15600b375df7da6f7918dd617
YauheniKazlou <zenka.2012@mail.ru>
2023-02-11 21:40:44 +0300

    ДР - Обработка исключений

--- New file.py
+++ New file.py
@@ -1,18 +1,10 @@
-n = input("Введите свой возраст:")
 
-if int(n) < 1:
-    print("Возраст введен неверно")
-elif int(n) > 99:
-    print("Возраст введен неверно")
-elif int(n) == 11:
-    print("Мне", n, "лет")
-elif int(n[-1]) == 1:
-    print("Мне", n, "год")
-elif int(n[-1]) == 2:
-    print("Мне", n, "года")
-elif int(n[-1]) == 3:
-    print("Мне", n, "года")
-elif int(n[-1]) == 4:
-    print("Мне", n, "года")
-else:
-    print("Мне", n, "лет")
+
+try:
+    chislo1 = input("Введите сегодняшний день недели: ")
+    chislo2 = input("Введите сегодняшнее число: ")
+    print("Сегодня: " + chislo1 + " " + int(chislo2) + "-го")
+except TypeError:
+    print("Используются разные типы данных в функции!")
+except ValueError:
+    print("Введена строка вместо цифр!")
