1069965094e5fc365b754faf02f4b4507eacff05
YauheniKazlou <zenka.2012@mail.ru>
2023-02-25 14:55:17 +0300

    ДР: вывести наибольшую цифру из введенного пятизначного натурального числа.

--- Цикл for (вывести наибольшую цифру из числа).py
+++ Цикл for (вывести наибольшую цифру из числа).py
@@ -2,13 +2,13 @@ try:
     chis = input("Введите пятизначное натуральное число: ")
     if len(chis) != 5:
         print("Число должно быть пятизначным!")
-    if int(chis[0]) >= (int(chis[1]) and int(chis[2]) and int(chis[3]) and int(chis[4])):
+    if int(chis[0]) >= (int(chis[1]) or int(chis[2]) or int(chis[3]) or int(chis[4])):
         print("Наибольшая цифра числа " + "\"" + str(chis) + "\"" + " - " + str(chis[0]))
-    elif int(chis[1]) >= (int(chis[0]) and int(chis[2]) and int(chis[3]) and int(chis[4])):
+    elif int(chis[1]) >= (int(chis[2]) or int(chis[3]) or int(chis[4])):
         print("Наибольшая цифра числа " + "\"" + str(chis) + "\"" + " - " + str(chis[1]))
-    elif int(chis[2]) >= (int(chis[1]) and int(chis[0]) and int(chis[3]) and int(chis[4])):
+    elif int(chis[2]) >= (int(chis[3]) or int(chis[4])):
         print("Наибольшая цифра числа " + "\"" + str(chis) + "\"" + " - " + str(chis[2]))
-    elif int(chis[3]) >= (int(chis[1]) and int(chis[2]) and int(chis[0]) and int(chis[4])):
+    elif int(chis[3]) >= int(chis[4]):
         print("Наибольшая цифра числа " + "\"" + str(chis) + "\"" + " - " + str(chis[3]))
     else:
         print("Наибольшая цифра числа " + "\"" + str(chis) + "\"" + " - " + str(chis[4]))
