4af73f7415db27adb1f28b1c9cbf32f62bf8ebab
YauheniKazlou <zenka.2012@mail.ru>
2023-02-26 01:19:09 +0300

    Нахождения чисел ряда Фибоначчи

--- Цикл for (факториал).py
+++ Цикл for (факториал).py
@@ -2,4 +2,4 @@ number = int(input("Введите число: "))
 factorial = 1
 for i in range(1, number+1):
     factorial *= i
-print("Факториал числа", "\""+str(number)+"\"", "равен", factorial)
+print("Факториал числа", "\""+str(number)+"\"", "равен -", factorial)
