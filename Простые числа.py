7c6b5e0a050cf4b86b781677a55dd61c4da839c0
YauheniKazlou <zenka.2012@mail.ru>
2023-02-28 09:49:31 +0300

    Вывести все простые числа во введенном диапазоне.

--- Простые числа.py
+++ Простые числа.py
@@ -1,5 +1,5 @@
 a = int(input("Введите диапазон простых чисел: "))
-
+print("Простые числа в диапазоне от 1 до", str(a) + ":")
 for i in range(1, a + 1):
     summ = 0
     for j in range(1, i + 1):
