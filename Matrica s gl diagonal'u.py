84b4a83300221795c5755d4acfbcef5a87417b7a
YauheniKazlou <zenka.2012@mail.ru>
2023-03-27 12:40:04 +0300

    Примеры матрицы. Создаем матрицу с главной диагональю.

--- Matrica s gl diagonal'u.py
+++ Matrica s gl diagonal'u.py
@@ -9,3 +9,17 @@ for i in range(n):
         else:
             A[i][j] = 1
 print(A)
+
+# Второй способ решения этой задачи
+n = int(input("Введите колличество строк и столбцов: "))
+A = []
+for i in range(n):
+    A[i][i] = 1
+for i in range(n):
+    for j in range(i + 1, n):
+        A[i][i] = 0
+for i in range(n):
+    for j in range(0, i):
+        A[i][i] = 2
+
+print(A)
