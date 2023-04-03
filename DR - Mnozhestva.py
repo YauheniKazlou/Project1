409546460de06e4b8b1f3e69744ecd683eadda56
YauheniKazlou <zenka.2012@mail.ru>
2023-04-03 13:47:11 +0300

    Задача №1: Вывел общие объекты обоих множеств. Задача №2: Создал n-кол-во множеств. Вывел множества, кратные трем и не входящие в первое множество.

---
+++ DR - Mnozhestva.py
@@ -0,0 +1,23 @@
+print("Задача №1:")
+comp1 = {"IT-Company", "ITT", "IT-Projekt", "IT-Overone"}
+comp2 = {"IT-Overone", "ITT", "Skillcompany"}
+print("Первое множество -", comp1)
+print("Второе множество -", comp2)
+print("Обшие объекты обоих множеств:", comp1.intersection(comp2))
+print("Задача №2:")
+import random
+n = int(input("Введите количество множеств: "))
+set1 = []
+for i in range(n):
+    set2 = set()
+    for j in range(random.randint(1, 3)):
+        set2.add(random.randrange(10))
+    set1.append(set2)
+print("", set1)
+print("Получившиеся множества, кратные трем и не входящие в первое множество:")
+for i in set1:
+    if len(i) == 3:
+        if set1[0].issuperset(i):
+            continue
+        else:
+            print("", i)
