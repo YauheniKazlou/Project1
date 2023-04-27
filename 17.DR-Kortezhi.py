1198b9b07f2a37a95f1739bffe4da2342ff10cf2
YauheniKazlou <zenka.2012@mail.ru>
2023-04-27 16:08:07 +0300

    ДР - дана строка с элементами (словами), необходимо сохранить эти слова как отдельные элементы в списке и вывести только первые символы (буквы) этих элементов.

--- 17.DR-Kortezhi.py
+++ 17.DR-Kortezhi.py
@@ -1,13 +1,8 @@
-st = 1, 3, 7, 9, 1, 3, 7, "Tom", 45, "Yan", "Tom", 1, 1
-print("Исходная строка -", st)
-str_spis = []
-for i in st:
-    str_spis.append(i)
-print("Исходный список -", str_spis)
-str_new = []
-for item in str_spis:
-    if item not in str_new:
-        str_new.append(item)
-print("Отредактированный список -", str_new)
-tuple1 = tuple(str_new)
-print("Кортеж -", tuple1)
+a = 'hello world'
+print(a)
+a1 = a.split()
+print(a1)
+r = []
+for i in a1:
+    r.append(i[0])
+print(tuple(r))
