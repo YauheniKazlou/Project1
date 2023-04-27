c8eeea4e4809709e40b80a7db99344433722894b
YauheniKazlou <zenka.2012@mail.ru>
2023-04-27 16:13:19 +0300

    Создал квадратную матрицу. Вывел сумму чисел главной диагонали. Посчитал колличество отрицательных чисел, расположенных ниже главной диагонали.

--- DR - Matrici.py
+++ DR - Matrici.py
@@ -1,8 +1,10 @@
 import random
 a = int(input("Введите коллличество строк и столбцов: "))
-A = [0] * a
+A = ["*"] * a
+print("Первое действие -", A)
 for i in range(a):
-    A[i] = [0] * a
+    A[i] = [1] * a
+    print(A[i])
 for i in range(a):
     for j in range(a):
         A[i][j] = random.randrange(-100, 100)
@@ -17,7 +19,7 @@ for i in range(a):
         elif i > j:
             if A[i][j] < 0:
                 A1.append(A[i][j])
-print("Сумма элементов главной диагонали равна -", S1)
+print("Сумма элементов главной диагонали равна ", S1)
 print("Колличество отрицательных чисел, расположенных ниже гл. диагонали равно -", len(A1))
 
 
