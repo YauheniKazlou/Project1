b0814cfad516df84c21b4b500057d2febc3ddcf7
YauheniKazlou <zenka.2012@mail.ru>
2023-04-28 18:54:29 +0300

    Вводим опред. кол-во чисел в список, оставляем только уникальние и сортируем этот список.

--- 20. Praktika. Mnozhestva i spiski.py
+++ 20. Praktika. Mnozhestva i spiski.py
@@ -2,10 +2,15 @@ n = int(input("Введите кол-во элементов списка: "))
 spis1 = []
 for i in range(n):
     spis1.append(input("-новый элемент: "))
-print(spis1)
+print("\nНачальный список -", spis1)
 spis2 = []
 for i in spis1:
     if i not in spis2:
         spis2.append(i)
 spis2.sort()
 print(spis2)
+
+print("\nВторой способ решения задачи:")  # используя метод перевода списка во множество и обратно в список
+sp = list(set(spis1))
+sp.sort()
+print(sp)
