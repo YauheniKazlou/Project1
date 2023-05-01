6874fa4c876b59601682096bd2c3d29d56532429
YauheniKazlou <zenka.2012@mail.ru>
2023-05-01 21:43:46 +0300

    Создаем словарь (англо-рус переводчик). При вводе слова, мы получаем его перевод.

--- 20.Praktika.Slovari.py
+++ 20.Praktika.Slovari.py
@@ -1,14 +1,11 @@
 slovar1 = {
     "hello": "привет",
     "book": "книга",
-    "world": "мир",
+    "world": "мир"
 }
 slovo = input("Введите слово: ")
 for key, val in slovar1.items():
-    if slovo in slovar1:
-        if slovo == key:
-            print(val)
-        elif slovo == val:
-            print(key)
-    else:
-        print("Введенное слово отсутствует в словаре!")
+    if slovo == key:
+        print(val)
+    elif slovo == val:
+        print(key)
